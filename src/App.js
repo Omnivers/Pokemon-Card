4747f00aea4bda9b9ddc48a605239675755aad8b
Omnivers <Twersimor.stock@outlook.com>
2022-08-02 14:46:56 +0200

    End

--- src/App.js
+++ src/App.js
@@ -1,25 +1,72 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from 'react'
+import "./App.css";
+import background from "./images/pokemon.gif";
+import defaultBackgroundImage from './images/pokemon.jpg';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+const App = () => { 
+  let random=1; // Pour le premier pokemon qui sera afficher dans notre page
+  const [pokemons, setPokemons] = useState(null);
+  const [ClassName, setClassName] = useState("card");
+  const fetchData = async () => {
+    const request = await fetch(`https://pokeapi.co/api/v2/pokemon/${random}`);//On utilise Random qu'on a défini au paravent pour definir l'ID
+    const response = await request.json()
+    setPokemons(response)
+  }
+  useEffect(()=>{
+    fetchData()
+  },[])
+
+
+//Functions :
+
+  const randomNumber= async ()=>{ //On click button lance à nouveau le fetch juste en dessous
+    random = Math.floor(Math.random() * 151) + 1 //Pour randomiser entre 1 et 151
+    const request = await fetch(`https://pokeapi.co/api/v2/pokemon/${random}`);
+    const response = await request.json()
+    setPokemons(response) // Montre le Pokemon randomisé
+  }
+  const changeBodyBackground=()=>{//Change le background après le onclick
+    document.body.style.backgroundImage = `url(${background})`;
+    setTimeout(defaultBackground,1000)
+  }
+  const defaultBackground=()=>{//Remet le background par defaut suite à l'affichage du pokemon random
+    document.body.style.backgroundImage= `url(${defaultBackgroundImage})`;
+    setClassName("card")
+  }
+  const animation =()=>{//ajoute une animation lors de l'affichage du pokemon
+    setClassName("card pokemon")
+  }
+  const capitalizeFirstLetter=(string) => {// Ajoute une maj au début de chaque string 
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  }
+
+
+  return(
+    <div className='container d-flex justify-content-center mt-4'>
+  {pokemons===null?(// condition qui :
+      <>
+      <div className='d-flex justify-content-center'>Loading</div> {/*Affiche un loading avant que la valeur de pokemon ne soit attribué */}
+      {console.log('test')}
+      </>
+  ):( // Sinon elle affiche notre pokemon :
+    <div className={ClassName} style={{width:'20rem'}}>
+    <img src={pokemons.sprites.other['official-artwork'].front_default} className="card-img-top" alt="..."/>
+    <div className="card-body">
+      <h5 className="card-title">{capitalizeFirstLetter(pokemons.name)}</h5>
+      <p className="card-text">Height : <span className='numero'>{pokemons.height}</span></p>
+      <p className="card-text">Weight : <span className='numero'>{pokemons.weight}</span></p>
+      <p className='card-text'>Type :</p>
+      {pokemons.types.map((pokemon)=>(// Type est un objet constitué de plusieur index du coup on le map pour afficher chaque type 
+         <li className="list-group-item list-group-item-action list-group-item-dark d-flex justify-content-center">{capitalizeFirstLetter(pokemon.type.name)}</li>
+         ))}
+      <a href="#" className="btn btn-outline-dark p-3 m-2 fs-4 text" onClick={()=>{randomNumber();changeBodyBackground();animation()} }>Show another random Pokemon</a> {/*Boutton pour randomiser la selection du Pokemon*/}
     </div>
-  );
-}
+    </div>
+  )}
+  </div>
+)}
 
-export default App;
+
+
+export default App
